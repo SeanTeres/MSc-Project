26c19699ba9bbc14594e0ae01e09a69b4619b6da
SeanTeres <seanterespolsky11@gmail.com>
2025-01-28 10:19:20 +0200

    fixed logic to use same train for models

--- classification/script.py
+++ classification/script.py
@@ -29,6 +29,20 @@ metadata_1 = pd.read_excel('MBOD_Datasets/Dataset 1/FileDatabaseWithRadiology.xl
 dicom_dir_2 = 'MBOD_Datasets/Dataset 2'
 metadata_2 = pd.read_excel('MBOD_Datasets/Dataset 2/Database_Training-2024.08.28.xlsx')
 
+# Initialize datasets
+d1 = classes.DICOMDataset1(dicom_dir=dicom_dir_1, metadata_df=metadata_1)
+d2 = classes.DICOMDataset2(dicom_dir=dicom_dir_2, metadata_df=metadata_2)
+
+# Split datasets and store indices
+train_indices_d1, val_indices_d1, test_indices_d1 = helpers.split_dataset(d1)
+train_indices_d2, val_indices_d2, test_indices_d2 = helpers.split_dataset(d2)
+
+# Save indices for later use
+split_indices = {
+    'd1': {'train': train_indices_d1, 'val': val_indices_d1, 'test': test_indices_d1},
+    'd2': {'train': train_indices_d2, 'val': val_indices_d2, 'test': test_indices_d2}
+}
+
 for experiment_name, experiment in config['experiments'].items():
     print(f"Running experiment: {experiment_name}")
     lr = experiment['lr']
@@ -40,8 +54,9 @@ for experiment_name, experiment in config['experiments'].items():
     resolution = experiment['resolution']
     oversampling = experiment['oversampling']
 
-    d1 = classes.DICOMDataset1(dicom_dir=dicom_dir_1, metadata_df=metadata_1, target_size=resolution, target_label=experiment['target_label'])
-    d2 = classes.DICOMDataset2(dicom_dir=dicom_dir_2, metadata_df=metadata_2, target_size=resolution, target_label=experiment['target_label'])
+    # Set target and resolution for datasets
+    d1.set_target(target, resolution)
+    d2.set_target(target, resolution)
 
     # Define augmentations as individual transforms
     augmentations_list = [
@@ -50,65 +65,48 @@ for experiment_name, experiment in config['experiments'].items():
         transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))  # Slight Blur
     ]
 
+    # Create dataloaders using stored indices
+    train_loader_d1, val_loader_d1, test_loader_d1 = helpers.create_dataloaders(d1, split_indices['d1'], batch_size, oversam=oversampling)
+    train_loader_d2, val_loader_d2, test_loader_d2 = helpers.create_dataloaders(d2, split_indices['d2'], batch_size, oversam=oversampling)
 
-    train_d1, augmented_train_d1, val_d1, test_d1, train_d2, augmented_train_d2, val_d2, test_d2 = helpers.read_data(d1, d2)
+    # Log oversampling information
+    if oversampling:
+        print("We are oversampling!!")
+
+    # Print label distribution
+    print("Label distribution for training set (D1):", helpers.calc_label_dist(d1, train_loader_d1.dataset, target))
+    print("Label distribution for validation set (D1):", helpers.calc_label_dist(d1, val_loader_d1.dataset, target))
+    print("Label distribution for test set (D1):", helpers.calc_label_dist(d1, test_loader_d1.dataset, target))
     
-    augmented_train_d1 = classes.AugmentedDataset(base_dataset=train_d1, augmentations_list=augmentations_list)
-    augmented_train_d2 = classes.AugmentedDataset(base_dataset=train_d2, augmentations_list=augmentations_list)
+    print("Label distribution for training set (D2):", helpers.calc_label_dist(d2, train_loader_d2.dataset, target))
+    print("Label distribution for validation set (D2):", helpers.calc_label_dist(d2, val_loader_d2.dataset, target))
+    print("Label distribution for test set (D2):", helpers.calc_label_dist(d2, test_loader_d2.dataset, target))
 
+    # Initialize wandb
+    wandb.login()
+    wandb.init(project='MBOD-2', name=experiment_name)
+    wandb.config.update(experiment)
+
+    # Initialize model
     model = xrv.models.DenseNet(weights=f"{model}")
     model = model.to(device)
-
     in_features = 1024  # Based on the output of model.features2()
     model.classifier = classes.BaseClassifier(in_features).to(device)
 
-    train_loader_d1, train_aug_loader_d1, val_loader_d1, test_loader_d1 = helpers.create_dataloaders(train_d1, augmented_train_d1, val_d1,
-                                                                                                test_d1, experiment['batch_size'])
-    
-    train_loader_d2, train_aug_loader_d2, val_loader_d2, test_loader_d2 = helpers.create_dataloaders(train_d2, augmented_train_d2, val_d2,
-                                                                                                test_d2, experiment['batch_size'])
-    
-    train_labels_d1 = d1.metadata_df.loc[train_d1.indices, 'Profusion Label']
-    train_labels_d2 = d2.metadata_df.loc[train_d2.indices, 'Profusion Label']
-
-    sample_weights_d1 = helpers.calculate_sample_weights(train_labels_d1)
-    sample_weights_d2 = helpers.calculate_sample_weights(train_labels_d2)
-
-    d1_sampler = WeightedRandomSampler(sample_weights_d1, len(sample_weights_d1), replacement=True)
-    d2_sampler = WeightedRandomSampler(sample_weights_d2, len(sample_weights_d2), replacement=True)
-
-    if (oversampling):
-        sample_weights_d1 = helpers.calculate_sample_weights(train_labels_d1)
-        sample_weights_d2 = helpers.calculate_sample_weights(train_labels_d2)
-
-        print("We are oversampling!!")
-        print(f"Sample Weights for Dataset 1: {np.unique(sample_weights_d1)}")
-        print(f"Sample Weights for Dataset 2: {np.unique(sample_weights_d2)}")
-
-
-        d1_sampler = WeightedRandomSampler(sample_weights_d1, len(sample_weights_d1), replacement=True)
-        d2_sampler = WeightedRandomSampler(sample_weights_d2, len(sample_weights_d2), replacement=True)
-        train_loader_d1 = DataLoader(train_d1, batch_size=batch_size, sampler=d1_sampler, pin_memory=True)
-        train_loader_d2 = DataLoader(train_d2, batch_size=batch_size, sampler=d2_sampler, pin_memory=True)
-    
-    wandb.login()
-    wandb.init(project='MBOD', name=experiment_name)
-    wandb.config.update(experiment)
-
-
-    
+    # Train and evaluate model
     test_labels_d1, test_labels_d2, test_preds_d1, test_preds_d2 = [], [], [], []
 
-    if(train_dataset == "MBOD 1"):
+    if train_dataset == "MBOD 1":
         print("Training on Dataset 1\n")
-        model = train_utils.train_model(train_loader_d1, val_loader_d1, model, n_epochs, lr, device)
+        model = train_utils.train_model(train_loader_d1, val_loader_d1, model, n_epochs, lr, device, loss_type=experiment['loss'])
     else:
         print("Training on Dataset 2\n")
-        model = train_utils.train_model(train_loader_d2, val_loader_d2, model, n_epochs, lr, device)
+        model = train_utils.train_model(train_loader_d2, val_loader_d2, model, n_epochs, lr, device, loss_type=experiment['loss'])
 
-    test_labels_d1, test_preds_d1 = train_utils.test_model(test_loader_d1, model, device, "MBOD 1")
-    test_labels_d2, test_preds_d2 = train_utils.test_model(test_loader_d2, model, device, "MBOD 2")
+    test_labels_d1, test_preds_d1 = train_utils.test_model(test_loader_d1, model, device, "MBOD 1", loss_type=experiment['loss'])
+    test_labels_d2, test_preds_d2 = train_utils.test_model(test_loader_d2, model, device, "MBOD 2", loss_type=experiment['loss'])
 
+    # Generate and save confusion matrices
     report_d1 = classification_report(test_labels_d1, test_preds_d1)
     cm_d1 = confusion_matrix(test_labels_d1, test_preds_d1)
 
@@ -121,16 +119,15 @@ for experiment_name, experiment in config['experiments'].items():
     print(f"Confusion Matrix ({experiment_name}- MBOD 1):")
     print(cm_d1)
 
-    print("*"*50)
+    print("*" * 50)
 
     print(f"Classification Report ({experiment_name} - MBOD 2)")
     print(report_d2)
 
-    print(f"Confusion Matrix ({experiment_name } - MBOD 2):")
+    print(f"Confusion Matrix ({experiment_name} - MBOD 2):")
     print(cm_d2)
 
-
-    plt.figure(figsize=(10,6))
+    plt.figure(figsize=(10, 6))
 
     plt.subplot(1, 2, 1)
     sns.heatmap(cm_d1, annot=True, fmt='d', cmap='Blues')
@@ -140,9 +137,6 @@ for experiment_name, experiment in config['experiments'].items():
     sns.heatmap(cm_d2, annot=True, fmt='d', cmap='Blues')
     plt.title(f"Confusion Matrix ({experiment_name}) - MBOD 2)")
 
-    plt.savefig(f"plots/conf_mat_{experiment_name}.png")
+    plt.savefig(f"new_plots/conf_mat_{experiment_name}.png")
 
     wandb.finish()
-
-    
-
