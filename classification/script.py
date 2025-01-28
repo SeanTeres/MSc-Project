2db57c01a40f9fb27a58bba4608df3083e5b5ec4
SeanTeres <seanterespolsky11@gmail.com>
2025-01-28 10:38:04 +0200

    logic fix

--- classification/script.py
+++ classification/script.py
@@ -33,15 +33,7 @@ metadata_2 = pd.read_excel('MBOD_Datasets/Dataset 2/Database_Training-2024.08.28
 d1 = classes.DICOMDataset1(dicom_dir=dicom_dir_1, metadata_df=metadata_1)
 d2 = classes.DICOMDataset2(dicom_dir=dicom_dir_2, metadata_df=metadata_2)
 
-# Split datasets and store indices
-train_indices_d1, val_indices_d1, test_indices_d1 = helpers.split_dataset(d1)
-train_indices_d2, val_indices_d2, test_indices_d2 = helpers.split_dataset(d2)
-
-# Save indices for later use
-split_indices = {
-    'd1': {'train': train_indices_d1, 'val': val_indices_d1, 'test': test_indices_d1},
-    'd2': {'train': train_indices_d2, 'val': val_indices_d2, 'test': test_indices_d2}
-}
+train_d1, augmented_train_d1, val_d1, test_d1, train_d2, augmented_train_d2, val_d2, test_d2 = helpers.read_data(d1, d2)
 
 for experiment_name, experiment in config['experiments'].items():
     print(f"Running experiment: {experiment_name}")
@@ -54,6 +46,7 @@ for experiment_name, experiment in config['experiments'].items():
     resolution = experiment['resolution']
     oversampling = experiment['oversampling']
 
+    print(f"MODEL :{model}")
     # Set target and resolution for datasets
     d1.set_target(target, resolution)
     d2.set_target(target, resolution)
@@ -64,32 +57,42 @@ for experiment_name, experiment in config['experiments'].items():
         transforms.RandomRotation(15),  # Rotate by 15 degrees
         transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0))  # Slight Blur
     ]
+    
+    augmented_train_d1 = classes.AugmentedDataset(base_dataset=train_d1, augmentations_list=augmentations_list)
+    augmented_train_d2 = classes.AugmentedDataset(base_dataset=train_d2, augmentations_list=augmentations_list)
 
-    # Create dataloaders using stored indices
-    train_loader_d1, val_loader_d1, test_loader_d1 = helpers.create_dataloaders(d1, split_indices['d1'], batch_size, oversam=oversampling)
-    train_loader_d2, val_loader_d2, test_loader_d2 = helpers.create_dataloaders(d2, split_indices['d2'], batch_size, oversam=oversampling)
+    model = xrv.models.DenseNet(weights="densenet121-res224-all")
+    model = model.to(device)
 
+    in_features = 1024  # Based on the output of model.features2()
+    model.classifier = classes.BaseClassifier(in_features).to(device)
+
+    # Create dataloaders
+    train_loader_d1, train_aug_loader_d1, val_loader_d1, test_loader_d1 = helpers.create_dataloaders(
+        train_d1, augmented_train_d1, val_d1, test_d1, experiment['batch_size'], oversam=experiment['oversampling']
+    )
+
+    train_loader_d2, train_aug_loader_d2, val_loader_d2, test_loader_d2 = helpers.create_dataloaders(
+        train_d2, augmented_train_d2, val_d2, test_d2, experiment['batch_size'], oversam=experiment['oversampling']
+    )
     # Log oversampling information
     if oversampling:
         print("We are oversampling!!")
 
     # Print label distribution
-    print("Label distribution for training set (D1):", helpers.calc_label_dist(d1, train_loader_d1.dataset, target))
-    print("Label distribution for validation set (D1):", helpers.calc_label_dist(d1, val_loader_d1.dataset, target))
-    print("Label distribution for test set (D1):", helpers.calc_label_dist(d1, test_loader_d1.dataset, target))
+    print("Label distribution for training set (D1):", helpers.calc_label_dist(d1, train_loader_d1.dataset, target + ' Label'))
+    print("Label distribution for validation set (D1):", helpers.calc_label_dist(d1, val_loader_d1.dataset, target + ' Label'))
+    print("Label distribution for test set (D1):", helpers.calc_label_dist(d1, test_loader_d1.dataset, target + ' Label'))
     
-    print("Label distribution for training set (D2):", helpers.calc_label_dist(d2, train_loader_d2.dataset, target))
-    print("Label distribution for validation set (D2):", helpers.calc_label_dist(d2, val_loader_d2.dataset, target))
-    print("Label distribution for test set (D2):", helpers.calc_label_dist(d2, test_loader_d2.dataset, target))
+    print("Label distribution for training set (D2):", helpers.calc_label_dist(d2, train_loader_d2.dataset, target + ' Label'))
+    print("Label distribution for validation set (D2):", helpers.calc_label_dist(d2, val_loader_d2.dataset, target + ' Label'))
+    print("Label distribution for test set (D2):", helpers.calc_label_dist(d2, test_loader_d2.dataset, target + ' Label'))
 
     # Initialize wandb
     wandb.login()
     wandb.init(project='MBOD-2', name=experiment_name)
     wandb.config.update(experiment)
 
-    # Initialize model
-    model = xrv.models.DenseNet(weights=f"{model}")
-    model = model.to(device)
     in_features = 1024  # Based on the output of model.features2()
     model.classifier = classes.BaseClassifier(in_features).to(device)
 
@@ -98,13 +101,13 @@ for experiment_name, experiment in config['experiments'].items():
 
     if train_dataset == "MBOD 1":
         print("Training on Dataset 1\n")
-        model = train_utils.train_model(train_loader_d1, val_loader_d1, model, n_epochs, lr, device, loss_type=experiment['loss'])
+        model = train_utils.train_model(train_loader_d1, val_loader_d1, model, n_epochs, lr, device)
     else:
         print("Training on Dataset 2\n")
-        model = train_utils.train_model(train_loader_d2, val_loader_d2, model, n_epochs, lr, device, loss_type=experiment['loss'])
+        model = train_utils.train_model(train_loader_d2, val_loader_d2, model, n_epochs, lr, device)
 
-    test_labels_d1, test_preds_d1 = train_utils.test_model(test_loader_d1, model, device, "MBOD 1", loss_type=experiment['loss'])
-    test_labels_d2, test_preds_d2 = train_utils.test_model(test_loader_d2, model, device, "MBOD 2", loss_type=experiment['loss'])
+    test_labels_d1, test_preds_d1 = train_utils.test_model(test_loader_d1, model, device, "MBOD 1")
+    test_labels_d2, test_preds_d2 = train_utils.test_model(test_loader_d2, model, device, "MBOD 2")
 
     # Generate and save confusion matrices
     report_d1 = classification_report(test_labels_d1, test_preds_d1)
